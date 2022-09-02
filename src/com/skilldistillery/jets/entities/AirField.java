fe8ee588252adaa8a910d16e8f74bf7de2dab5c1
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-09-02 16:39:40 -0600

    initial

---
+++ src/com/skilldistillery/jets/entities/AirField.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.jets.entities;
+
+import java.util.List;
+
+public class AirField {
+	private List<Jets> airfield;
+
+}
+
+
+
+
+//add
+//remove in here?
+//this interacts with JetsApplication
+
